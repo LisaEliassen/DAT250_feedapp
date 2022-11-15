6579275aa8636f7bc58332d1b55452cf742440f4
Lisa Maria Eliassen <lisaeli@hotmail.no>
2022-11-15 19:57:04 +0100

    Continued frontend implementation..

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import Home from './Home';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Polls from './Polls';
-import Polls2 from './Polls2'
 import Vote from './Vote'
 
 class App extends Component {
@@ -13,7 +12,6 @@ class App extends Component {
                 <Route exact path='/' component={Home}/>
                 <Route path='/polls' exact={true} component={Polls}/>
                 <Route path='/vote/:id' component={Vote}/>
-                <Route path='/polls/:id' component={Polls2}/>
             </Switch>
         </Router>
     )
