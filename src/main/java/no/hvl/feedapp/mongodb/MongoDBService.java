54a8a89a3c2fba4fdbe76c3ec6d696b4cef560c9
Olesya Pasichnyk <olesya2011@hotmail.no>
2022-11-26 11:29:15 +0100

    Implemented a MongoDBService and deleted the other mongoDB files we don't need

--- src/main/java/no/hvl/feedapp/mongodb/MongoDBService.java
+++ src/main/java/no/hvl/feedapp/mongodb/MongoDBService.java
@@ -1,44 +1,86 @@
 package no.hvl.feedapp.mongodb;
 
+import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoCommandException;
 import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
 import no.hvl.feedapp.model.Poll;
 import org.bson.Document;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MongoDBService {
 
-    public void connectToDatabase() {
+    private MongoDatabase database;
+
+    public MongoDBService () {
         try (var mongoClient = MongoClients.create("mongodb://localhost:27017")) {
 
             // Must use mongosh first (command: use testDB) if database not yet created
             String myDatabase = "testDB";
-            var database = mongoClient.getDatabase(myDatabase);
+            this.database = mongoClient.getDatabase(myDatabase);
 
-            System.out.println("database name -> " + database.getName());
+            System.out.println("database name -> " + this.database.getName());
 
             // Print the collection names inside the database
-            for (String name: database.listCollectionNames()) {
+            for (String name: this.database.listCollectionNames()) {
 
                 System.out.println(name);
             }
         }
+
     }
 
-    public void createCollection(String collectionName) {
-        // TODO: implement
+    public MongoDatabase getDatabase() {
+        return database;
     }
 
-    public void readCollection(String collectionName) {
-        // TODO: implement
+    public void setDatabase(MongoDatabase database) {
+        this.database = database;
     }
 
-    public void dropCollection(String collection) {
-        // TODO: implement
+    public void dropDB(String myDatabase) {
+        // Drop it
+        this.database.drop();
+        System.out.println("Database which is dropped: " + myDatabase );
     }
 
-    public void dropDB() {
-        // TODO: implement
+    public void createCollection(String myCollection) {
+        try {
+            // Name the collection
+            this.database.createCollection(myCollection);
+            System.out.println("Collection created successfully");
+        } catch (MongoCommandException e) {
+            this.database.getCollection(myCollection).drop();
+        }
+    }
+
+    public void readCollection(String myCollection) {
+        MongoCollection <Document> collection = this.database.getCollection(myCollection);
+
+        try (MongoCursor < Document > cur = collection.find().iterator()) {
+            while (cur.hasNext()) {
+                var doc = cur.next();
+                var polls = new ArrayList < > (doc.values());
+
+                System.out.printf("PollID: %s. Title: %s. Result: %s.%n", polls.get(0), polls.get(1), polls.get(2));
+            }
+        }
+    }
+
+    public void dropCollection(String myCollection) {
+        // Get a collection
+        MongoCollection<Document> collection = this.database.getCollection(myCollection);
+        System.out.println("Collection name -> " + collection);
+
+        // Dropping a Collection
+        collection.drop();
+        System.out.println("Collection dropped successfully");
     }
 
     public Document createDoc(Poll poll) {
@@ -48,12 +90,50 @@ public class MongoDBService {
         return doc;
     }
 
-    public void addDocument (Document document) {
-        // TODO: implement
+    public void addOneDocument (Document document, String myCollection) {
+        // Get the collection and populate it with data
+        MongoCollection <Document> collection = this.database.getCollection(myCollection);
+        collection.insertOne(document); // if it's one you can use collection.insertOne(docs)
     }
 
-    public void deleteDocument(String collection) {
-        // TODO: implement
+    public void addManyDocuments (ArrayList<Document> docs, String myCollection) {
+        // Get the collection and populate it with data
+        MongoCollection <Document> collection = this.database.getCollection(myCollection);
+        collection.insertMany(docs);
+    }
+
+    public void deleteDocument(String myCollection, Long PollID) {
+        // Retrieving a collection
+        MongoCollection <Document> collection = this.database.getCollection(myCollection);
+
+        // Deleting the document with certain pollID
+        collection.deleteOne(Filters.eq("pollID", PollID));
+        System.out.println("Document deleted successfully...");
+    }
+
+    public Document getDocument(String myCollection, Long pollID) {
+        MongoCollection <Document> collection = this.database.getCollection(myCollection);
+        Document doc = collection.find(new Document("PollID", pollID)).first();
+        return doc;
+    }
+
+    public void updateDocument(String myCollection, Document newDocument, Document oldDocument) {
+        // Retrieving a collection
+        MongoCollection <Document> collection = this.database.getCollection(myCollection);
+
+        // Updating one collection
+        collection.updateOne(oldDocument,
+                new Document("$set", newDocument));
+
+        // Retrieving the documents after updating
+        try (MongoCursor <Document> cur = collection.find().iterator()) {
+            while (cur.hasNext()) {
+                var doc = cur.next();
+                var polls = new ArrayList < > (doc.values());
+
+                System.out.printf("PollID: %s. Title: %s. Result: %s.%n", polls.get(0), polls.get(1), polls.get(2));
+            }
+        }
     }
 
 }
