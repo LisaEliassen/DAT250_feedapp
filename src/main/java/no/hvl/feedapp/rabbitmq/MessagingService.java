accf207c6160afd7bd911423fa0e0efc68b5b222
Olesya Pasichnyk <olesya2011@hotmail.no>
2022-11-28 11:25:15 +0100

    TopicMessaging

--- src/main/java/no/hvl/feedapp/rabbitmq/MessagingService.java
+++ src/main/java/no/hvl/feedapp/rabbitmq/MessagingService.java
@@ -3,7 +3,8 @@ package no.hvl.feedapp.rabbitmq;
 public class MessagingService {
 
     public static void main(String[] args) throws Exception {
-        ReceiveLogs.main(args);
-        EmitLog.main(args);
+        String[] argv = {"Hello", "World", "YOLO"};
+        ReceiveLogsTopic.main(argv);
+        EmitLogTopic.main(argv);
     }
 }
